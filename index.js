6e662d50a04e15fbae23c6108edf1fd29306b7e7
KabongeHajarah <kabongehajarah@gmail.com>
2023-08-06 23:58:40 +0400

    Functionality with js

--- index.js
+++ index.js
@@ -8,3 +8,16 @@ const jsxElement = <h1>This is a JSX element</h1>
 const rootElement = document.getElementById('root')
 
 ReactDOM.render(jsxElement, rootElement)
+
+const title = <h2>Getting Started React</h2>
+
+const header = (
+    <header>
+      <h1>Welcome to 30 Days Of React</h1>
+      <h2>Getting Started React</h2>
+      <h3>JavaScript Library</h3>
+    </header>
+  )
+ 
+
+ReactDOM.render(header, rootElement)
